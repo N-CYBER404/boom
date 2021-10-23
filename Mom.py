406a98c6f7190d412c2d9ea3ba7693eab61afb28
N-CYBER404 <93034151+N-CYBER404@users.noreply.github.com>
2021-10-23 21:38:51 +0600

    Mom.py

--- Mom.py
+++ Mom.py
@@ -58,12 +58,12 @@ logo="""
 
 
 --------------------------------------------------
->>AUTHER=====>𝐌𝐫. 𝐍𝐈𝐊𝐈
->>GITHUB =====>Niki-Cyber404
->>FACEBOOK===>Niki.Cyber404
->>INSTAGRAM==>niki.cyber404
->>YOUTUBE====>JAMES NIKI
->>TELEGRAM===>@Niki_Cyber404
+AUTHER=====>𝐌𝐫. 𝐍𝐈𝐊𝐈
+GITHUB =====>Niki-Cyber404
+FACEBOOK===>Niki.Cyber404
+INSTAGRAM==>niki.cyber404
+YOUTUBE====>JAMES NIKI
+TELEGRAM===>@Niki_Cyber404
 --------------------------------------------------
                                 """
 
